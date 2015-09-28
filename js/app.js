b36860bf0c477f873b3649fe32b8d97965a54792
Andreas Gustafsson <=>
2015-09-28 22:28:50 +0200

    Init commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+var app = angular.module('hello-world', []);
+
+//will be the file for all the configuration for your application
---
+++ js/controllers/HomeCtrl.js
@@ -0,0 +1,3 @@
+angular.module('hello-world').controller('HomeCtrl', function($scope){
+	
+});
